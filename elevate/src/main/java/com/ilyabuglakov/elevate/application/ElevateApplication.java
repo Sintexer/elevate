0eb909293a8f08dafa3293aa564f61d0e3feecea
Илья Буглаков <Neonlightnight@gmail.com>
2020-11-29 23:24:43 +0300

    added spring initializr

---
+++ elevate/src/main/java/com/ilyabuglakov/elevate/application/ElevateApplication.java
@@ -0,0 +1,13 @@
+package com.ilyabuglakov.elevate.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevateApplication.class, args);
+	}
+
+}
---
+++ elevate/src/test/java/com/ilyabuglakov/elevate/ElevateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyabuglakov.elevate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
