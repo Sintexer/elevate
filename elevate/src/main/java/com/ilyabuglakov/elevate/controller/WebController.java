a13b7d901453989aba38ad2d79a1e50e481c9591
Илья Буглаков <Neonlightnight@gmail.com>
2020-11-30 20:34:52 +0300

    updated navbar

--- elevate/src/main/java/com/ilyabuglakov/elevate/controller/WebController.java
+++ elevate/src/main/java/com/ilyabuglakov/elevate/controller/WebController.java
@@ -1,15 +1,19 @@
 package com.ilyabuglakov.elevate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/")
 public class WebController {
 
     @GetMapping
-    public String index(){
+    public String index(Model model, Principal principal){
+        model.addAttribute("user", principal);
         return "index";
     }
 
