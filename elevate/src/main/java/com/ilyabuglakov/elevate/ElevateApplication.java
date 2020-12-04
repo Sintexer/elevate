b134d592cd712aff2d0fc89f3c662dbcddc6aeb2
Илья Буглаков <Neonlightnight@gmail.com>
2020-12-04 15:06:09 +0300

    fixed images

--- elevate/src/main/java/com/ilyabuglakov/elevate/ElevateApplication.java
+++ elevate/src/main/java/com/ilyabuglakov/elevate/ElevateApplication.java
@@ -2,12 +2,20 @@ package com.ilyabuglakov.elevate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class ElevateApplication {
+public class ElevateApplication extends WebMvcConfigurationSupport  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElevateApplication.class, args);
 	}
 
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+		super.addResourceHandlers(registry);
+	}
 }
